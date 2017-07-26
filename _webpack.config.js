e99bdd4c519e3f338a10fa3e74d33bc16a197bfa
Juan Gabriel <tuxgabo@gmail.com>
2017-07-25 22:52:12 -0500

    new bundle

--- _webpack.config.js
+++ _webpack.config.js
@@ -47,29 +47,7 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': '"production"'
     }),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      mangle: true,
-      compress: {
-        warnings: false, // Suppress uglification warnings
-        pure_getters: true,
-        unsafe: true,
-        unsafe_comps: true,
-        screw_ie8: true
-      },
-      output: {
-        comments: false,
-      },
-      exclude: [/\.min\.js$/gi] // skip pre-minified libs
-    }),
-    new webpack.IgnorePlugin(/^\.\/locale$/, [/moment$/]),
-    new webpack.NoErrorsPlugin(),
-    new CompressionPlugin({
-      asset: "[path].gz[query]",
-      algorithm: "gzip",
-      test: /\.js$|\.css$|\.html$/,
-      threshold: 10240,
-      minRatio: 0
-    })
+   new webpack.optimize.UglifyJsPlugin(),
+   new webpack.IgnorePlugin(/^\.\/locale$/, [/moment$/])
   ]
 };
