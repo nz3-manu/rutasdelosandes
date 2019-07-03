d87ecf2aa17bb02fec7501e769235df51eb8cd4c
Juan Gabriel <tuxgabo@gmail.com>
2019-07-03 17:30:30 -0500

    shopify store name updated

--- graphql-js-client.js
+++ graphql-js-client.js
@@ -3,9 +3,10 @@ import Client from 'graphql-js-client';
 import types from './schema/types';
 
 global.fetch = fetch;
+let shopifyStoreName = "randes-store";
 
 export default new Client(types, {
-  url: 'https://randex.myshopify.com/api/graphql',
+  url: `https://${shopifyStoreName}.myshopify.com/api/graphql`,
   fetcherOptions: {
     headers: {
       'X-Shopify-Storefront-Access-Token': 'e61c79b2a527a6255dd60c3233ef2a28'
