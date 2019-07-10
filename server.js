ca7e5356e0d57c553979ba19c74393a6828c9671
Juan Gabriel <tuxgabo@gmail.com>
2019-07-10 11:20:50 -0500

    let's fix it latter

--- server.js
+++ server.js
@@ -339,7 +339,7 @@ app.get('/amp/producto/:slug', async (req, res) => {
   let price = new Intl.NumberFormat("de-DE", {
                 style: 'currency',
                 currency: defaultChild.priceV2.currencyCode,
-                maximumFractionDigits:0
+                maximumFractionDigits: 1
               }).format(defaultChild.priceV2.amount);
   
   let variationsParams = shopifyVariations.map((variantObj) => variantObj.name).reduce(
