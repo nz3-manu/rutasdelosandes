d34277bd031fdbcb4e2da1577ce1654594b07f66
Juan Gabriel <tuxgabo@gmail.com>
2019-07-15 09:18:22 -0500

    price v2

--- server.js
+++ server.js
@@ -348,7 +348,7 @@ app.get('/amp/producto/:slug', async (req, res) => {
   }, {});
 
   let defaultChild = shopifyProduct.data.productByHandle.variants.edges[0].node;
-  
+  console.log("price V2", defaultChild.priceV2.amount)
   let price = new Intl.NumberFormat("de-DE", {
                 style: 'currency',
                 currency: defaultChild.priceV2.currencyCode
