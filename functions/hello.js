2046b0de1db3c5c7c6a7d9cc921e980da0b44304
Juan Ramirez <juan.ramirez@mail.rockstarcoders.com>
2020-04-10 12:26:43 -0500

    syntax error fixed

--- functions/hello.js
+++ functions/hello.js
@@ -1,8 +1,8 @@
 // this uses the callback syntax, however, we encourage you to try the async/await syntax shown in async-dadjoke.js
-export function handler(event, context, callback) {
+exports.handler = function(event, context, callback) {
   console.log("queryStringParameters", event.queryStringParameters);
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({ msg: "Hello, World!" })
   });
-}
+};
