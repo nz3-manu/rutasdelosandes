07ee947bb8a228a0c01141f478031facdd2ca38e
Juan Ramirez <juan.ramirez@mail.rockstarcoders.com>
2020-04-12 15:42:31 -0500

    documents json is outside of the landa folder

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 // set this var for react inner components
 global.__preloaded__ = JSON.parse(
-  fs.readFileSync("./_site/documents.json", "utf8")
+  fs.readFileSync("../_site/documents.json", "utf8")
 );
 // mocking shopify responses
 global.__mocking__ = true;
