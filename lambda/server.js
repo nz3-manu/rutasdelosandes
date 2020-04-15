5c216c70bdb4121170388e84e11c72f0d1341dae
Juan Ramirez <juan.ramirez@mail.rockstarcoders.com>
2020-04-15 11:02:18 -0500

    new attemp

--- lambda/server.js
+++ lambda/server.js
@@ -70,7 +70,9 @@ import { Provider } from "react-redux";
 import { log } from "util";
 // set this var for react inner components
 //console.log(fs.readFileSync("./func/documents.json", "utf8"));
-//global.__preloaded__ = JSON.parse(fs.readFileSync("/_site/documents.json", "utf8"));
+global.__preloaded__ = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, "_site/documents.json"), "utf8")
+);
 // mocking shopify responses
 global.__mocking__ = true;
 
