70739c0017705e465732a2e3a5d99f88319bb434
Juan Ramirez <juan.ramirez@mail.rockstarcoders.com>
2020-04-16 12:07:08 -0500

    server side render is here

--- lambda/server.js
+++ lambda/server.js
@@ -42,17 +42,7 @@ Sentry.init({
 });
 const documents = require("../_site/documents.json");
 // i think this is cousing the errors
-//app.use(Sentry.Handlers.requestHandler());
-//app.use(Sentry.Handlers.errorHandler());
-//// Optional fallthrough error handler
-//app.use(function onError(err, req, res, next) {
-//// The error id is attached to `res.sentry` to be returned
-//// and optionally displayed to the user for support.
-//res.statusCode = 500;
-//res.end(res.sentry + '\n');
-//});
-
-app.set("views", "./views");
+app.set("views", "../views");
 app.set("view engine", "ejs");
 
 require("es6-promise").polyfill();
@@ -60,17 +50,15 @@ require("isomorphic-fetch");
 console.log("server is actually running");
 
 //server side fetch polifyll
-//import routes from "./_javascript/routes";
+import routes from "../_javascript/routes";
 import { match, RouterContext } from "react-router";
-//import { read, write, push, sendToDevice, update, remove } from "./chatbot/db";
-//import { Promise } from "firebase";
 import reducer from "../_javascript/reducers";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import { log } from "util";
-// set this var for react inner components
-//console.log(fs.readFileSync("./func/documents.json", "utf8"));
 global.__preloaded__ = documents;
+//import { read, write, push, sendToDevice, update, remove } from "./chatbot/db";
+//import { Promise } from "firebase";
 // mocking shopify responses
 global.__mocking__ = true;
 
@@ -82,11 +70,193 @@ app.use(
   })
 );
 
+// if not a static file come to react router
 router.get(`*`, (req, res) => {
-  console.log("visitando", req.url);
-  res.json(documents);
+  try {
+    let ampEquivalent = false;
+
+    if (req.originalUrl.match(/[a-z/].html[-a-zA-Z0-9()@:%_\+.~#?&//=]*/)) {
+      ampEquivalent = `${req.protocol}://${req.get(
+        "host"
+      )}/amp${req.originalUrl.split("?").shift()}`;
+    }
+
+    let cartOpen = req.query.cartOpen;
+
+    mathRouter(
+      req,
+      res,
+      { products: [], cart: { number: 0, items: [], open: cartOpen } },
+      ampEquivalent
+    );
+  } catch (error) {
+    console.log(error);
+  }
 });
 
+function mathRouter(req, res, state = {}, ampEquivalent) {
+  // material ui stylesheet server
+  // Create a sheetsRegistry instance.
+  const sheetsRegistry = new SheetsRegistry();
+
+  // Create a theme instance.
+  const theme = createMuiTheme({
+    palette: {
+      primary: blueGrey,
+      type: "light"
+    }
+  });
+  const generateClassName = createGenerateClassName();
+  // end of material ui server stylesheet
+  let store = createStore(
+    reducer,
+    Object.assign({}, global.__preloaded__, state)
+  );
+
+  const preloadedState = store.getState();
+  match({ routes: routes, location: req.url }, (err, redirect, props) => {
+    // in here we can make some decisions all at once
+    if (err) {
+      // there was an error somewhere during route matching
+      res.status(500).send(err.message);
+    } else if (redirect) {
+      // we haven't talked about `onEnter` hooks on routes, but before a
+      // route is entered, it can redirect. Here we handle on the server.
+      res.redirect(redirect.pathname + redirect.search);
+    } else if (props) {
+      let content;
+      // if we got props then we matched a route and can render
+      content = ReactDOMServer.renderToString(
+        <JssProvider
+          registry={sheetsRegistry}
+          generateClassName={generateClassName}
+        >
+          <MuiThemeProvider theme={theme} sheetsManager={new Map()}>
+            <Provider store={store}>
+              <RouterContext {...props} />
+            </Provider>
+          </MuiThemeProvider>
+        </JssProvider>
+      );
+      // Grab the CSS from our sheetsRegistry.
+      const css = sheetsRegistry.toString();
+      const fullPage = renderFullPage(
+        content,
+        preloadedState,
+        "",
+        css,
+        ampEquivalent,
+        req.url.split("?").shift()
+      );
+      if (typeof fullPage != "number") {
+        res.send(fullPage);
+      } else {
+        res.status(fullPage).sendFile(__dirname + "/_site/404.html");
+      }
+    } else {
+      // no errors, no redirect, we just didn't match anything
+      res.status(404).send("Ruta no encontrada ");
+    }
+  });
+}
+
+function renderFullPage(
+  html,
+  preloadedState,
+  customHtml = "",
+  customCSS = "",
+  ampEquivalent = false,
+  reqUrl
+) {
+  let Analytics = ``;
+  let RegisterSW = ``;
+  let amptag = ``;
+  let structuredData = ``;
+  if (process.env.NODE_ENV == "production") {
+    RegisterSW = `if ('serviceWorker' in navigator) {
+										navigator.serviceWorker.register('/service-worker.js');
+									}`;
+    Analytics = `<script async src="https://www.googletagmanager.com/gtag/js?id=UA-100391485-2"></script>
+                  <script>
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', 'UA-100391485-2', { 'dataSource': 'REACT', 'use_amp_client_id': true });
+                  </script>`;
+  }
+
+  if (ampEquivalent) {
+    var ampDoc;
+    try {
+      ampDoc = fs.readFileSync(`./_site/amp${decodeURI(reqUrl)}`, "utf8");
+    } catch (err) {
+      return 404;
+    }
+
+    const $ = cheerio.load(ampDoc);
+    structuredData = $('script[type="application/ld+json"]').html();
+    $("link[rel=canonical]").remove();
+    $("style").remove();
+    $("script").remove();
+    $("noscript").remove();
+    $("amp-analytics").remove();
+
+    amptag = `
+      ${$("head").html()}
+      <link rel="amphtml" href="${ampEquivalent}">
+    `;
+  }
+
+  return `
+	  <!doctype html>
+	  <html>
+		<head>
+      ${amptag}
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta property="fb:pages" content="1078600055607267" />
+                        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <meta name="google-site-verification" content="NI1CzFN9-ZqzNWWYGfh8a_28Ee4atbyWwDRuS9nwwm4" />
+      <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+      <style id="jss-server-side">${customCSS}</style>
+			${Analytics}
+			<style>
+			${fs.readFileSync("./_includes/styles.html", "utf8")}
+      </style>
+      <!-- Facebook Pixel Code -->
+      <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '171238663763950');
+        fbq('track', 'PageView');
+      </script>
+      <noscript><img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=171238663763950&ev=PageView&noscript=1"
+      /></noscript>
+      <!-- End Facebook Pixel Code -->
+                        <!-- Asynchronously load the AMP-with-Shadow-DOM runtime library. -->
+                        <script async src="https://cdn.ampproject.org/shadow-v0.js"></script>
+                      </head>
+    <body>
+    <script type="application/ld+json">
+      ${structuredData}
+    </script>
+    <script>
+      window.__preloaded__ = ${JSON.stringify(preloadedState)}
+      ${RegisterSW}
+    </script>
+		  ${customHtml}
+		  <div id="root">${html}</div>
+		  <script src="/javascript/index.bundle.js"></script>
+		</body>
+	  </html>
+	  `;
+}
 var functionName = "server";
 // Set router base path for local dev
 const routerBasePath =
