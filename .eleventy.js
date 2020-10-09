a97b0a322dbde9a0e49928b4ce2f93e79a0e45a5
Juan Ramirez <tuxgabo@gmail.com>
2020-10-08 21:14:59 -0500

    eleventy taking it place in the build

--- .eleventy.js
+++ .eleventy.js
@@ -17,6 +17,7 @@ module.exports = function (eleventyConfig) {
   });
 
   eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("documents.json");
 
   return {
     dir: {
