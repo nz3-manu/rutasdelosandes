8a2636fb571edf8607994d46bd946827490bd261
Juan Ramirez <tuxgabo@gmail.com>
2020-10-10 12:09:31 -0500

    passing js

--- .eleventy.js
+++ .eleventy.js
@@ -17,6 +17,7 @@ module.exports = function (eleventyConfig) {
   });
 
   eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("javascript");
   eleventyConfig.addPassthroughCopy("documents.json");
 
   return {
