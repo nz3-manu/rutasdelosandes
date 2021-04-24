0af4d89f1f439773558d9883fef7860cd81d22b7
gabriel <tuxgabo@gmail.com>
2021-04-24 10:14:54 -0500

    moving towards a more standard routes

--- _javascript/components/amp-document/amp-document.js
+++ _javascript/components/amp-document/amp-document.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "isomorphic-fetch";
 import { withRouter } from "react-router";
 import PushBanner from "../push-banner";
 
@@ -81,6 +82,7 @@ class AMPDocument extends React.Component {
   }
 
   render() {
+    const { staticContext } = this.props;
     if (this.state.offline) {
       return (
         <div>
@@ -137,7 +139,6 @@ class AMPDocument extends React.Component {
    */
   fetchAndAttachAmpDoc_(url) {
     this.setState({ loading: true });
-    console.log("JG url to fetch", url);
     this.fetchDocument_(url)
       .then((doc) => {
         this.hideUnwantedElementsOnDocument_(doc);
@@ -181,39 +182,51 @@ class AMPDocument extends React.Component {
    * @return {!Promise<!Document|!string>} If fetch succeeds, resolved with {!Document}.
    *         Otherwise, rejects with {!string} error description.
    */
-  fetchDocument_(url) {
-    return new Promise((resolve, reject) => {
-      this.xhr_ = new XMLHttpRequest();
-      this.xhr_.open("GET", url, true);
-      this.xhr_.responseType = "document";
-      this.xhr_.setRequestHeader("Accept", "text/html");
-      this.xhr_.onreadystatechange = () => {
-        if (this.xhr_.readyState < /* STATUS_RECEIVED */ 2) {
-          return;
-        }
-        if (this.xhr_.status < 100 || this.xhr_.status > 599) {
-          this.xhr_.onreadystatechange = null;
-          reject(new Error(`Unknown HTTP status ${this.xhr_.status}`));
-          this.xhr_ = null;
-          return;
-        }
-        if (this.xhr_.readyState === /* COMPLETE */ 4) {
-          if (this.xhr_.responseXML) {
-            resolve(this.xhr_.responseXML);
-          } else {
-            reject(new Error("No xhr.responseXML"));
-          }
-          this.xhr_ = null;
-        }
-      };
-      this.xhr_.onerror = () => {
-        reject(new Error("Network failure"));
-      };
-      this.xhr_.onabort = () => {
-        reject(new Error("Request aborted"));
-      };
-      this.xhr_.send();
-    });
+  async fetchDocument_(url) {
+    let response;
+    let html;
+    try {
+      response = await fetch(url);
+      html = await response.text();
+    } catch (e) {
+      /* handle error */
+    } finally {
+      /* be executed regardless of the try / catch result*/
+    }
+    return html;
+
+    //new Promise((resolve, reject) => {
+    //this.xhr_ = new XMLHttpRequest();
+    //this.xhr_.open("GET", url, true);
+    //this.xhr_.responseType = "document";
+    //this.xhr_.setRequestHeader("Accept", "text/html");
+    //this.xhr_.onreadystatechange = () => {
+    //if (this.xhr_.readyState < [> STATUS_RECEIVED <] 2) {
+    //return;
+    //}
+    //if (this.xhr_.status < 100 || this.xhr_.status > 599) {
+    //this.xhr_.onreadystatechange = null;
+    //reject(new Error(`Unknown HTTP status ${this.xhr_.status}`));
+    //this.xhr_ = null;
+    //return;
+    //}
+    //if (this.xhr_.readyState === [> COMPLETE <] 4) {
+    //if (this.xhr_.responseXML) {
+    //resolve(this.xhr_.responseXML);
+    //} else {
+    //reject(new Error("No xhr.responseXML"));
+    //}
+    //this.xhr_ = null;
+    //}
+    //};
+    //this.xhr_.onerror = () => {
+    //reject(new Error("Network failure"));
+    //};
+    //this.xhr_.onabort = () => {
+    //reject(new Error("Request aborted"));
+    //};
+    //this.xhr_.send();
+    //});
   }
   trackEvents(elem) {
     let GAeventsData = {
---
+++ _javascript/routes.js
@@ -0,0 +1,77 @@
+import React from "react";
+import AMPDocument from "./components/amp-document/amp-document";
+import Shell from "./components/shell";
+import Checkout from "./components/checkout";
+import Confirmation from "./components/order-confirmation";
+import Products from "./components/products";
+import About from "./components/about";
+import Politicas from "./components/politicas";
+import Blog from "./components/blog";
+import Regions from "./components/regions";
+import GenericNotFound from "./components/404";
+import OrderList from "./components/orderlist";
+
+import loadData from "./helpers/loadData";
+
+//<Route path="/blog" component={Blog} />
+//<Route path="/tienda" component={Products} />
+//<Route path="/contacto" component={About} />
+//<Route path="/politicas" component={Politicas} />
+//<Route path="/confirmation" component={Confirmation} />
+//<Route path="/orderslist" component={OrderList} />
+//<Route path="*" exact={true} component={GenericNotFound} />
+/**
+ * @see https://github.com/ampproject/amphtml/blob/master/extensions/amp-install-serviceworker/amp-install-serviceworker.md#shell-url-rewrite
+ */
+function redirectSWFallbackURL(_nextState, replace) {
+  var hash = typeof window !== "undefined" && window.location.hash;
+  if (hash && hash.indexOf("#href=") === 0) {
+    var href = decodeURIComponent(hash.substr(6));
+    replace({ pathname: href });
+  }
+}
+
+const Routes = [
+  {
+    path: "/",
+    component: Shell,
+    onEnter: redirectSWFallbackURL,
+  },
+  {
+    path: "/regiones",
+    component: Regions,
+  },
+  {
+    path: "/blog",
+    component: Blog,
+  },
+  {
+    path: "/tienda",
+    component: Blog,
+  },
+  {
+    path: ":category/:document",
+    component: (props) => (
+      <AMPDocument
+        src={`/amp/${props.params.category}/${props.params.document}`}
+      />
+    ),
+  },
+  {
+    path: ":category/:deparment/:document",
+    component: (props) => (
+      <AMPDocument
+        src={`/amp/${props.params.category}/${props.params.deparment}/${props.params.document}`}
+      />
+    ),
+  },
+  {
+    path: "/checkout",
+    component: (props) => <Checkout query={props.location.query} />,
+  },
+  {
+    component: GenericNotFound,
+  },
+];
+
+export default Routes;
