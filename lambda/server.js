3f13ce8740857378db8a934257be57d5cca4e892
Manuel Ramirez <manuelramirez@MBP-de-Manuel.domain.name>
2022-04-05 12:44:20 -0500

    instal googlr tags

--- lambda/server.js
+++ lambda/server.js
@@ -550,6 +550,13 @@ function renderFullPage(
         ${globalStyles}
       </style>
       <meta property="article:publisher" content="{{site.url}}">
+      <!-- Google Tag Manager -->
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-NCXJMLQ');</script>
+        <!-- End Google Tag Manager -->
       <!-- facebook metadata -->
       <meta property="og:title"       content="${docMetaData.title}">
       <meta property="og:url"         content="{{site.url}}{{page.url}}">
